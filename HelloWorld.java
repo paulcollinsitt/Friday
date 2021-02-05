14c92a425db0a48c567e417371f131f9e4a2ad69
Paul Collins <paul.collins@ittralee.ie>
2021-02-05 14:34:51 +0000

    Feb 5 at 2.34
    I did things

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+// HelloWorld.java
+// Paul Collins - Semester 1 - Lab 1
+/* Write "Hello World!" on the screen */
+//
+
+public class HelloWorld {
+	
+	public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+    
+}
+
+
